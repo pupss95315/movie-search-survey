e01055b0062c90c70c57e5e2f2c5404f6d91fa65
pupss95315 <45054745+pupss95315@users.noreply.github.com>
2024-08-18 14:59:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://github.com/hms5232/random-survey';
+window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://pupss95315.github.io/movie-search-survey';
